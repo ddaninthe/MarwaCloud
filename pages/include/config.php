c274e4369f8df8826ac8ab3e6476432b9379c1e8
ddaninthe <d.daninthelopez@gmail.com>
2019-01-16 12:10:31 +0100

    Fixed bootstrap alert

--- pages/include/config.php
+++ pages/include/config.php
@@ -6,6 +6,5 @@
         const JWT_SECRET = "keepYourSecretSafe";
     }
     
-    //$apiKey = "b8069a7897a86981acc378f63bb32ebbf07c9d93";
     //"x-api-key": "18ba364730addbc2611ec25dc71f927c0aafaa35"
 ?>
